3fddaf79944170035d4d52ac2d3106fb5e403274
jerry955071 <jerry955071@gmail.com>
2022-09-13 16:33:40 +0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
 from setuptools import setup, find_packages
 
+# read README.md
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="nanoprep-ccc",
-    version="0.0.3",
+    version="0.0.1",
     author="Chia-Chen Chu",
     author_email="jerry955071@gmail.com",
     description="A fully-equipped, fast, and memory-efficient pre-processor for ONT transcriptomic data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jerry955071/NanoPreP",
+    url="https://github.com/Woodformation1136/NanoPreP",
     packages=find_packages(),
-    install_requires=[
-        "edlib>=1.3.8",
-        "numpy"
-        ],
-    entry_points={
-        'console_scripts': [
-            'nanoprep = NanoPreP.__main__:main',
-        ]
-    }
+    install_requires=["edlib>=1.3.8", "numpy"],
+    entry_points={"console_scripts": ["nanoprep = NanoPreP.main:main"]}
 )
