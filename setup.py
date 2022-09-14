e253defc3b1e1bee06f672b7ec4ed33bf46e6206
jerry955071 <jerry955071@gmail.com>
2022-09-14 18:30:11 +0800

    fixed entry point

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="nanoprep-ffm",
-    version="0.0.3",
+    version="0.0.4",
     author="Chia-Chen Chu",
     author_email="jerry955071@gmail.com",
     description="A fully-equipped, fast, and memory-efficient pre-processor for ONT transcriptomic data",
@@ -15,5 +15,5 @@ setup(
     url="https://github.com/Woodformation1136/NanoPreP",
     packages=find_packages(),
     install_requires=["edlib>=1.3.8", "numpy"],
-    entry_points={"console_scripts": ["nanoprep = NanoPreP.main:main"]}
+    entry_points={"console_scripts": ["nanoprep = NanoPreP.__main__:main"]}
 )
