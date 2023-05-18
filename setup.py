ee90ac32dfa23fc056e9a272db068c876b590239
jerry955071 <jerry955071@gmail.com>
2023-05-18 14:36:21 +0800

    added `plotly` to dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="nanoprep-ffm",
-    version="0.0.5",
+    version="0.0.11",
     author="Chia-Chen Chu",
     author_email="jerry955071@gmail.com",
     description="A fully-equipped, fast, and memory-efficient pre-processor for ONT transcriptomic data",
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Woodformation1136/NanoPreP",
     packages=find_packages(),
-    install_requires=["edlib>=1.3.8", "numpy"],
+    install_requires=["edlib>=1.3.8", "numpy", "plotly"],
     entry_points={
         "console_scripts": [
             "nanoprep = NanoPreP.__main__:main",
