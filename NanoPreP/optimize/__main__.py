2c55f66bc1b346352b0b3ff6ae6a01d785ac409e
jerry955071 <jerry955071@gmail.com>
2023-05-25 20:34:39 +0800

    modularized optimzation processes

--- NanoPreP/optimize/__main__.py
+++ NanoPreP/optimize/__main__.py
@@ -1,6 +1,7 @@
 from NanoPreP.optimize.argParser import parser, Defaults
 from NanoPreP.preptools.Annotator import Optimizer
 from NanoPreP.seqtools.FastqIO import FastqIO, SeqFastq
+from NanoPreP.optimize.template import template as html_template
 from collections import Counter
 from pathlib import Path
 from plotly.subplots import make_subplots
@@ -18,132 +19,125 @@ def openg(p:Path, mode:str):
         return gzip.open(p, mode + "t")
     else:
         return open(p, mode)
-
-def main():
-    # parse arguments
-    args = parser.parse_args()
     
-    # collect parameters
-    params = Defaults.copy()
     
-    # get parameter from config
-    if args.config:
-        config = json.load(open(args.config))
-        params.update(config)
-        
-    # get command line arguments from ArgumentParser
-    for k, v in vars(args).items():
-        if v:
-            params[k] = v
-
-    
+# get primer identity distribution
+def get_pid_counts(
+        p5_sense:str,
+        p3_sense:str,
+        isl5:tuple,
+        isl3:tuple,
+        input_fq:str,
+        n:int,
+        skip_short:int,
+        skip_lowq:int
+    ):
     # initiate Annotator object
     optimizer = Optimizer(
-        p5_sense = params["p5_sense"],
-        p3_sense = params["p3_sense"],
-        isl5 = params["isl5"],
-        isl3 = params["isl3"],
+        p5_sense =p5_sense,
+        p3_sense = p3_sense,
+        isl5 = isl5,
+        isl3 = isl3,
     )
-    type1, type2 = "ISL", "Read body"
+    
+    # initiate counters
     counters = {
-        type1: Counter(),
-        type2: Counter()
+        "ISL": Counter(),
+        "Read body": Counter()
     }
     
-    
-    # Color palette
-    palatte = {
-        type1: "#FFAF33",
-        type2: "#2190FF"
-    }
-    
-    
-    #
-    with openg(params["input_fq"], "r") as handle_in:
+    # iterate through reads
+    with openg(input_fq, "r") as handle_in:
         # Sample at most `n` reads
         SAMPLED = 0
         for read in FastqIO.read(handle_in):
             # skip too-short reads if `skip_short`
-            if params["skip_short"] > len(read):
+            if skip_short > len(read):
                 continue
             
             # skip low-quality reads if `skip_lowq` 
-            if params["skip_lowq"] > SeqFastq.meanq(read):
+            if skip_lowq > SeqFastq.meanq(read):
                 continue
             
             
             SAMPLED += 1
-            if SAMPLED > params["n"]:
+            if SAMPLED > n:
                 SAMPLED -= 1
                 break
             
             # primer alignment
             for _, (pid_isl, pid_rb) in optimizer.alignInB(read).items():
-                counters[type1][round(pid_isl, 2)] += 1
-                counters[type2][round(pid_rb, 2)] += 1
+                counters["ISL"][round(pid_isl, 2)] += 1
+                counters["Read body"][round(pid_rb, 2)] += 1
         
 
-    # Plot sampling results
-    pids = set(list(counters[type1].keys())) | set(list(counters[type2].keys()))
+    # add zero counts
+    pids = set(list(counters["ISL"].keys())) | set(list(counters["Read body"].keys()))
     pids = list(pids)
     for pid in pids:
-        counters[type1][pid] += 0
-        counters[type2][pid] += 0
+        counters["ISL"][pid] += 0
+        counters["Read body"][pid] += 0
         
+    return SAMPLED, counters
+
+
+def plot_pid_counts(SAMPLED, counters, palatte, output, params, html_template):
     # Create figure
     fig = make_subplots(
         rows=1,
         cols=1,
         subplot_titles=[f"Sampled {SAMPLED:,} reads"]
     )
-    
-    # Add trace `type2` data to subplot (1, 1)
+        
+    # Add trace `"Read body"` data to subplot (1, 1)
     fig.add_trace(
         go.Bar(
-            x=[f"{i:.2f}" for i in counters[type2].keys()],
-            y=[v for k, v in counters[type2].items()],
-            name=f"{type2}",
-            marker_color=palatte[type2],
+            x=[f"{i:.2f}" for i in counters["Read body"].keys()],
+            y=[v for k, v in counters["Read body"].items()],
+            name="Read body",
+            marker_color=palatte["Read body"],
             opacity=.8,
             legendrank=2
         ),
         row=1, col=1
     )
-    
-    # Add trace `type1` data
+        
+    # Add trace `ISL` data
     fig.add_trace(
         go.Bar(
             visible=True,
-            x=[f"{i:.2f}" for i in counters[type1].keys()],
-            y=[v for k, v in counters[type1].items()],
-            name=f"{type1}",
-            marker_color=palatte[type1],
+            x=[f"{i:.2f}" for i in counters["ISL"].keys()],
+            y=[v for k, v in counters["ISL"].items()],
+            name="ISL",
+            marker_color=palatte["ISL"],
             opacity=.8,
             legendrank=1
         ),
         row=1, col=1
     )
-    
+        
 
     # legendrank = 3
+    pids = set(list(counters["ISL"].keys())) | set(list(counters["Read body"].keys()))
+    pids = list(pids)
     hvtexts = []
     for pid in pids:
-        tp = sum([v if k >= pid else 0 for k, v in counters[type1].items()])
-        fp = sum([v if k >= pid else 0 for k, v in counters[type2].items()])
+        tp = sum([v if k >= pid else 0 for k, v in counters["ISL"].items()])
+        fp = sum([v if k >= pid else 0 for k, v in counters["Read body"].items()])
         precision = round(tp / (tp + fp), 3)
         hvtexts.append(
             f"True positives: {tp}<br>" +
             f"False positives: {fp}<br>" +
             f"Precision: {precision}"
         )
-        
+            
     fig.add_trace(
         go.Bar(
             visible=True,
             x=[f"{i:.2f}" for i in pids],
             y=[max([
                 max(fig["data"][0]["y"]),
-                max(fig["data"][0]["y"])
+                max(fig["data"][1]["y"])
             ])] * len(pids),
             marker_color="red",
             opacity=0,
@@ -184,55 +178,79 @@ def main():
     
     
     # makedir
-    os.makedirs(Path(params["output"]).parent, exist_ok=True)
-    with open(params["output"], "w") as handle_html:
-        out = """\
-<html>
-    <head><meta charset="utf-8" /></head>
-    <style type="text/css">
-        .gap-20 { 
-            width:100%; 
-            height:20px; 
-        }
-        .gap-10 { 
-            width:100%; 
-            height:10px; 
-        } 
-        #title {
-            font-family: Sans-serif;
-            font-size: 24px;
-            font-weight: 600;
-        }
-        #text {
-            font-family: Sans-serif;
-            font-size: 16px;
-            font-weight: 600;
-        }
-        #comment {
-            font-family: Sans-serif;
-            font-size: 12px;
-        }
-        #code {
-            font-family: Monospace;
-            font-size: 14px;
-        }
-    </style>
-    <body>
-        <div id="title">NanoPreP-optimize</div>
-        <div class="gap-20"></div>
-        <div>%(plot)</div>
-        <div id="text">Command line:</div>
-        <div id="code">nanoprep-optimize %(args)</div>
-        <div class="gap-10"></div>
-        <div id="text">Parameters:</div>
-        <div id="code">%(params)</div>
-    </body>
-</html>"""
+    os.makedirs(Path(output).parent, exist_ok=True)
+    with open(output, "w") as handle_html:
+        out = html_template
         out = out.replace("%(args)", " ".join(sys.argv[1:]))
         out = out.replace("%(plot)", fig.to_html())
         out = out.replace("%(params)", json.dumps(params))
         handle_html.write(out)
     return
 
+
+def get_pid_cutoff(counters, precision_cutoff):
+    # get pid cutoff
+    pid_cutoff = None
+    
+    # iter over pids
+    pids = set(list(counters["ISL"].keys())) | set(list(counters["Read body"].keys()))
+    pids = list(pids)
+    pids.sort(reverse=True)
+    for pid in pids:
+        tp = sum([v if k >= pid else 0 for k, v in counters["ISL"].items()])
+        fp = sum([v if k >= pid else 0 for k, v in counters["Read body"].items()])
+        precision = round(tp / (tp + fp), 3)
+        if precision >= precision_cutoff:
+            pid_cutoff = pid
+        else:
+            break
+
+    return pid_cutoff
+
+
+def main():    
+    # get parameters
+    params = Defaults.copy()
+    
+    # parse arguments
+    args = parser.parse_args()
+    
+    # update parameter from config
+    if args.config:
+        config = json.load(open(args.config))
+        params.update(config)
+        
+    # update command line arguments from ArgumentParser
+    for k, v in vars(args).items():
+        if v:
+            params[k] = v
+            
+    
+    # get pid counts
+    SAMPLED, counters = get_pid_counts(
+        p5_sense=params["p5_sense"],
+        p3_sense=params["p3_sense"],
+        isl5=params["isl5"],
+        isl3=params["isl3"],
+        input_fq=params["input_fq"],
+        n=params["n"],
+        skip_short=params["skip_short"],
+        skip_lowq=params["skip_lowq"]
+    )
+
+
+    # plot
+    plot_pid_counts(
+        SAMPLED=SAMPLED,
+        counters=counters,
+        palatte={
+            "ISL": "#FFAF33",
+            "Read body": "#2190FF"
+        },
+        output=params["output"],
+        params=params,
+        html_template=html_template
+    )
+    
 if __name__ == "__main__":
     main()
